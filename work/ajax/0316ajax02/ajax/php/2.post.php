9e36b2917c2f10ad75ffd67b09d900d15895d0e4
zhangpingping-123 <2569228717@qq.com>
2020-05-15 19:03:10 +0800

    ajax

---
+++ work/ajax/0316ajax02/ajax/php/2.post.php
@@ -0,0 +1,9 @@
+﻿<?php
+	header('content-text:text/html; charset="utf-8"');
+	error_reporting(0);
+	header("Access-Control-Allow-Origin:*");
+		
+	$username = $_POST['username'];
+	$age	  = $_POST['age'];
+	
+	echo "欢迎xxxx。。。名字:{$username} , 年龄: {$age}"; 
