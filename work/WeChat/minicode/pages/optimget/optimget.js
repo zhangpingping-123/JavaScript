16fcb1e8d83f05996c8e76386a1ed3e53abf7fe8
zhangpingping-123 <2569228717@qq.com>
2020-05-18 20:42:16 +0800

    5-18

--- work/WeChat/minicode/pages/optimget/optimget.js
+++ work/WeChat/minicode/pages/optimget/optimget.js
@@ -7,6 +7,11 @@ Page({
   data: {
 
   },
+  handleClickWithParams:function(e){
+    console.log("id",e.currentTarget.dataset.id),
+    console.log("num",e.currentTarget.dataset.num)
+    console.log("number",e.currentTarget.dataset.number)
+  },
 
   /**
    * 生命周期函数--监听页面加载
